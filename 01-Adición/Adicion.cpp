8b366c8905e1afca22de0694b2d10481abf6c74f
mdominguez01 <63793189+mdominguez01@users.noreply.github.com>
2020-04-27 00:36:29 -0300

    Add files via upload

---
+++ 01-Adición/Adicion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int a,b;
+    std::cout << "Primer nro: ";
+    std::cin >> a;
+    std::cout << "Segundo nro: ";
+    std::cin >> b;
+    std::cout << "La suma es: " << a+b;
+}
