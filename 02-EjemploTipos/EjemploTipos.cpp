808171570741ed5d256aeb56c1370d929d52be4e
mdominguez01 <63793189+mdominguez01@users.noreply.github.com>
2020-05-21 11:49:00 -0300

    Update EjemploTipos.cpp

--- 02-EjemploTipos/EjemploTipos.cpp
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -15,7 +15,7 @@ int main(){
     // Ejemplificacion Tipos de Datos
 
     bool booleano=false;
-    unsigned long long int nro_positivo_lli=999999999999999;
+    unsigned long long int nro_positivo_lli=999998;
     short nro_short=2;
     int entero=-5;
     double nro_double=12.3456;
@@ -29,6 +29,37 @@ int main(){
     assert(nro_double==12.3456);
     assert(caracter!='b');
     assert(cadena=="hola");
-
+    
+    
+    //operaciones con booleanos
+    
+    
+    //operaciones con unsign long init,init y short (usan las mismas operaciones)
+    //SUMA
+     nro_positivo_lli= nro_positivo_lli +1;
+     assert(nro_positivo_lli==999999);
+    //RESTA
+     nro_short= nro_short - 1;
+     assert(nro_short==1);
+    //multiplicacion
+     entero= entero*1;
+     assert(entero==-5);
+    //division
+     nro_positivo_lli= nro_positivo_lli/1;
+     assert(nro_positivo_lli==999999);
+     
+     //operaciones con double
+    //SUMA
+     nro_double= nro_double +1;
+     assert(nro_double==13.3456);
+    //RESTA
+     nro_double= nro_double - 1.0;
+     assert(nro_double==12.3456);
+    //multiplicaion
+     nro_double= nro_double*1;
+     assert(nro_double==12.3456);
+    //division
+     nro_double= nro_double/1;
+     assert(nro_double==12.3456);
 
 }
