b17d7889c1c64d73be00f332f1238452233257e1
mdominguez01 <63793189+mdominguez01@users.noreply.github.com>
2020-06-11 00:50:31 -0300

    Add files via upload

---
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -0,0 +1,26 @@
+/*
+ Trabajo practico N°5 IsBisiesto
+ Dado un año, determinar si es bisiesto
+ Alumno: Matias Dominguez
+ Legajo: 1743375
+ 11-06-2020
+*/
+#include <cassert>
+
+bool IsBisiesto (int); //prototipo
+
+int main(){
+
+    //prueba
+    assert(IsBisiesto(2000));
+    assert(not IsBisiesto(2019));
+    assert(IsBisiesto(1908));
+    assert(not IsBisiesto(1910));
+    assert(IsBisiesto(1584));
+
+}
+
+//definicion
+bool IsBisiesto (int ano){
+    return ano>=1582 and (ano%4==0 and ano%100!=0 or ano%400==0);
+} 
