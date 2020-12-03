15821e0407f64d20f73e626151def6e8e6a21d4b
mdominguez01 <63793189+mdominguez01@users.noreply.github.com>
2020-12-03 19:10:32 -0300

    Add files via upload

---
+++ 06-TrabajoFinal/PoligonoListasEnlazadas/PoligonoListasEnlazadas.cpp
@@ -0,0 +1,277 @@
+/*
+ Trabajo practico N°6 Trabajo Final
+ Poligono  ListasEnlazadas
+ Alumno: Matias Dominguez
+ Legajo: 1743375
+ 03-12-2020
+*/
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include "PoligonoListasEnlazadas.h"
+
+using namespace std;
+
+void CopiarPoligonosConPerimetrosMayoresA (double min, string nombreArchivoIn, string nombreArchivoOut){
+    
+    Poligonos poligonos;
+    ifstream in(nombreArchivoIn+".txt");
+    ExtraerPoligonosSegunPerimetros(poligonos,in,min);
+
+    ofstream theFile(nombreArchivoOut+".txt");
+    InsertarPoligonos(poligonos,theFile);
+    theFile.close();
+
+}
+
+
+bool ExtraerPoligonosSegunPerimetros(Poligonos &poligonos, istream &in, double min_p){
+
+    poligonos.n=0;
+    Poligono n;
+    int place=0;
+    do
+    {
+        ExtraerPoligono(n, in);
+        double perimetroN=GetPerimetro(n);    
+        if(min_p < perimetroN ){
+            poligonos.losPoligonos.at(place)=n;
+            poligonos.n++;
+            
+            if(!in.eof())place++;
+        }
+        
+    } while (!in.eof());
+
+    return in.eof();
+
+}
+
+bool ExtraerPoligonos(Poligonos &poligonos, istream &in){
+    Poligono n;
+    int place=0;
+    do
+    {
+        ExtraerPoligono(n, in);
+        poligonos.losPoligonos.at(place)=n;
+        poligonos.n=place+1;
+        
+        if(!in.eof())place++;
+
+    } while (!in.eof());
+
+    return in.eof();
+
+}
+
+bool ExtraerPoligono(Poligono &n, istream &in){
+    unsigned nro=0;
+    ExtraerColor(n.color,in);
+    in >> nro;
+    n.n=0;
+    do
+    {
+        Punto punto;
+        ExtraerPunto(punto,in);
+        AddVertice(n,punto);
+        //n.n++;
+        nro--;
+    } while (nro>0);
+    //n.n--;
+    return in.eof();
+}
+
+
+bool ExtraerColor(Color &color, istream &in){
+    int nro;
+    in >> nro;
+    color.r = (uint8_t) nro;
+    in >> nro;
+    color.g = (uint8_t) nro;
+    in >> nro;
+    color.b = (uint8_t) nro;
+
+    return in.eof();
+}
+
+
+bool ExtraerPunto(Punto &p, istream &in){
+    
+    in >> p.x;
+    in >> p.y;
+    return in.eof();
+}
+
+
+
+bool InsertarPoligonos(const Poligonos &poligonos, ostream &out){
+    if(poligonos.n==0)return out.eof();
+    int place=0;
+    do
+    {
+        InsertarPoligono(poligonos.losPoligonos.at(place), out);
+        if(place<(poligonos.n-1)){
+            out << " ";
+            place++;
+        }else
+        {
+            place++;
+        }
+        out << "\n";
+    } while ( place<poligonos.n);
+
+    return out.eof();
+
+}
+
+bool InsertarPoligono(const Poligono &n, ostream &out){
+    unsigned nro=1;
+    InsertarColor(n.color,out);
+    out << n.n;
+    do
+    {
+        Punto punto = GetVertice(n,nro);
+        InsertarPunto(punto,out);
+        nro++;
+    } while (nro<=n.n);
+    
+    return out.eof();
+}
+
+bool InsertarColor(const Color &color, ostream &out){
+    
+    out << (unsigned)color.r << " ";
+    out << (unsigned)color.g << " ";
+    out << (unsigned)color.b << " ";
+
+    return out.eof();
+}
+
+bool InsertarPunto(const Punto &punto, ostream &out){
+    out << " " << punto.x << " ";
+    out << punto.y ;
+    return out.eof();
+}
+
+
+
+//FUnciones de un Punto
+
+bool isIgual(const Punto &a, const Punto &b)
+{
+
+    return (a.x == b.x) && (a.y == b.y) ? true : false;
+}
+
+double GetDistacia(const Punto &a, const Punto &b)
+{
+    double d = sqrt(pow((b.x - a.x), 2.0) + pow((b.y - a.y), 2.0));
+    return d;
+}
+
+double GetDistanciaAlOrigen(const Punto &a)
+{
+    Punto x{0,0};
+    return GetDistacia(x,a);
+}
+
+//Funiones de un Poligono
+
+void AddVertice(Poligono &a, Punto puntoNuevo)
+{
+    a.n = a.n + 1;
+
+    auto aux = new NodoPunto;
+    aux->p = puntoNuevo;
+    aux->next = a.nodosPuntosPoligono;
+    a.nodosPuntosPoligono = aux;
+
+}
+
+Punto GetVertice(const Poligono &a, unsigned n2)
+{
+    auto punto = a.nodosPuntosPoligono;
+    unsigned total= a.n;
+    while(total>0){
+        if(total==n2){
+            return punto->p;
+        }
+        punto = punto->next;
+        total--;
+    };
+    return punto->p;
+}
+
+
+void SetVertice(Poligono &a, unsigned n2, Punto puntoNuevo)
+{
+    auto punto = a.nodosPuntosPoligono;
+    unsigned total= a.n;
+    while(total>0){
+        if(total==n2){
+            punto->p = puntoNuevo;
+        }
+        punto = punto->next;
+        total--;
+    };
+}
+
+void RemoveVertice(Poligono &a, unsigned n2)
+{
+    auto aux1 = new NodoPunto;
+    auto aux2 = new NodoPunto;
+    auto aux3 = new NodoPunto;
+    auto punto = a.nodosPuntosPoligono;
+    unsigned total= a.n;
+    
+    if(total!=n2){
+        while(total>0){
+            if(total==(n2+1)){
+                aux1 = punto;
+            }
+            if(total==n2){
+                aux2 = punto;
+            }
+            if(total==(n2-1)){
+                aux3 = punto;
+            }
+            punto = punto->next;
+                total--;
+        };
+        aux1->next = aux3;
+    }else{
+         while(total>0){
+            if(total==n2){
+                aux2 = punto;
+            }
+            if(total==(n2-1)){
+                aux3 = punto;
+            }
+            punto = punto->next;
+             total--;
+         }
+         a.nodosPuntosPoligono = aux3;
+    }
+
+    a.n--;
+
+}
+
+unsigned GetCantidadLados(const Poligono &a)
+{
+    return a.n;
+}
+
+double GetPerimetro(const Poligono &a)
+{
+    double perimetro = 0;
+    for (unsigned i = 1; i <= a.n; i++)
+    {
+        perimetro = i < a.n ? perimetro + GetDistacia(GetVertice(a,i), GetVertice(a,(i+1))) : perimetro + GetDistacia(GetVertice(a,i), GetVertice(a,1));
+    }
+
+    return perimetro;
+}
+
---
+++ 06-TrabajoFinal/PoligonoListasEnlazadas/PoligonoListasEnlazadasTest.cpp
@@ -0,0 +1,92 @@
+/*
+ Trabajo practico N°6 Trabajo Final
+ Poligono  ListasEnlazadas
+ Alumno: Matias Dominguez
+ Legajo: 1743375
+ 03-12-2020
+*/
+
+#include <fstream>
+#include <iostream>
+#include <cassert>
+#include "PoligonoListasEnlazadas.h"
+
+using namespace std;
+
+int main()
+{
+    Punto uno = {-2.0, 2.0};
+    Punto dos = {2.0, 2.0};
+    Punto tres = {2.0, -2.0};
+    Punto cuatro = {-2.0, -2.0};
+
+
+    Poligono poli{nullptr, 0, {0, 0, 0}};
+
+    AddVertice(poli, uno);
+    AddVertice(poli, dos);
+    AddVertice(poli, tres);
+    AddVertice(poli, cuatro);
+
+    //asserts del Poligono
+    //Get Vertice
+    assert(isIgual(uno, GetVertice(poli, 1)));
+    assert(isIgual(dos, GetVertice(poli, 2)));
+    assert(!isIgual(tres, GetVertice(poli, 1)));
+    assert(!isIgual(cuatro, GetVertice(poli, 2)));
+
+    //Add vetice
+    Punto cinco{-10, 0};
+    AddVertice(poli, cinco);
+    assert(isIgual(cinco, GetVertice(poli, 5)));
+    assert(!isIgual(tres, GetVertice(poli, 5)));
+
+    //Set vetice
+    SetVertice(poli, 5, uno);
+    assert(!isIgual(cinco, GetVertice(poli, 5)));
+    assert(isIgual(uno, GetVertice(poli, 5)));
+
+    //Catidad de lados
+    assert(4 != GetCantidadLados(poli));
+    assert(5 == GetCantidadLados(poli));
+
+    //Remover vetice
+    RemoveVertice(poli, 5);
+    assert(isIgual(cuatro, GetVertice(poli, poli.n)));
+    assert(!isIgual(cuatro, GetVertice(poli, 3)));
+
+    //Get_Perimetro
+    assert(15.9 < GetPerimetro(poli) && 16.1 > GetPerimetro(poli));
+    assert(!(19.4 < GetPerimetro(poli) && 19.5 > GetPerimetro(poli)));
+
+
+    // Creacion de los archivos
+    
+    ofstream myfile("poligono.txt");
+    myfile << "255 255 255 5 0.2 2.0 1.2 3.0 2.1 4.0 -1.0 8.0 -8.0 2.0\n0 0 0 4 2.0 2.0 -2.0 2.0 2.0 -2.0 -2.0 -2.0";
+    myfile.close();
+
+    ofstream file("p2.txt");
+    file << "255 255 255 5 0.2 2.0 1.2 3.0 2.1 4.0 -1.0 8.0 -8.0 2.0\n0 0 0 4 2.0 2.0 -2.0 2.0 -2.0 -2.0 2.0 -2.0";
+    file.close();
+    
+    // ExtaerPoligonos, InsertarPoligonos 
+
+    Poligonos poligonos;
+    Poligonos poligonos2;
+  
+    ifstream in("poligono.txt");
+    ifstream in2("p2.txt");
+    
+    assert(ExtraerPoligonos(poligonos,in));
+    assert(ExtraerPoligonos(poligonos2,in2));
+    
+    assert(!InsertarPoligonos(poligonos,cout));
+    assert(!InsertarPoligonos(poligonos2,cout));
+
+    // CopiarPoligonosConPerimetrosMayoresA
+
+    CopiarPoligonosConPerimetrosMayoresA(20.0, "documentoPoligonos","salida");
+
+    return 0;
+}
